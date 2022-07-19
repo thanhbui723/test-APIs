7b391523dbfdba8f458d96711e7befc5af711ef3
THANH BÙI VĂN <18120559@student.hcmus.edu.vn>
2022-07-19 11:13:18 +0700

    commit update and delete user

--- PostgreSQL exercise/users/routes.js
+++ PostgreSQL exercise/users/routes.js
@@ -9,7 +9,7 @@ router.post('/create-user', async (req, res) => {
     INSERT INTO users(name, age, address)
     VALUES ('${body.name}', ${body.age}, '${body.address}')
   `);
-  res.send(result);
+  res.status(201).send('add user successfully');
 });
 
 router.get('/', async(req, res) => {
@@ -21,7 +21,22 @@ router.get('/', async(req, res) => {
 });
 
 // TODO: Update user with parameter user's id
+router.put('/update-user/:id',async (req,res) => {
+  const id = parseInt(req.params.id);
+  const body = req.body;
+  const result = await client.query(
+    'UPDATE users SET name = $1,age = $2, address = $3 WHERE id=$4',
+    [body.name,body.age,body.address,id]);
+  res.status(201).send('update user successfully');
+})
 
 // TODO: Delete user with parameter user's id
-
+router.delete('/delete-user/:id', async(req,res)=>{
+  const id = parseInt(req.params.id);
+  const body = req.body;
+  const result = await client.query(
+    'DELETE FROM users WHERE id = $1',[id]
+  );
+  res.status(201).send('delete user successfully');
+})
 module.exports = router;
